93e67af63638ec98a9a8f7d7e350811dee8953df
CyanCoding <camden.macleod@pm.me>
2021-07-07 08:24:30 -0400

    Add file reader and greeting

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func ReadWordsFile(path string) []string {
+	byteData, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		fmt.Println("Invalid path!")
+		return nil
+	}
+
+	text := string(byteData)
+	words := strings.Fields(text)
+
+	return words
+}
+
 func main() {
+	for {
+		fmt.Print("Please enter a text file path > ")
+		var path string
+		fmt.Scanln(&path)
+
+		fmt.Println()
+		fmt.Println("Reading file...")
+		wordList := ReadWordsFile(path)
+
+		if wordList != nil {
+			fmt.Println("Finished reading file of ", len(wordList), " lines")
+			break
+		}
+	}
 
 }
