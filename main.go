bb064ebc04e58202192d127fd093923903c81e74
CyanCoding <camden.macleod@pm.me>
2021-07-07 08:24:43 -0400

    Add action selector

--- main.go
+++ main.go
@@ -36,4 +36,24 @@ func main() {
 		}
 	}
 
+	fmt.Println("Available options:")
+	fmt.Println("(1) Remove all lines with capital letters")
+	fmt.Println("(2) Remove all lines with lowercase letters")
+	fmt.Println("(3) Remove all lines with a certain character")
+	fmt.Println("(4) Remove all lines without a certain character")
+	fmt.Println("------------------------------------------------")
+	for {
+		fmt.Print("Action number > ")
+		var action int = 0
+		fmt.Scanln(&action)
+
+		if action > 0 && action < 5 { // Action is valid
+			break
+		} else {
+			fmt.Println()
+			fmt.Println("Invalid action!")
+			fmt.Println()
+		}
+	}
+
 }
